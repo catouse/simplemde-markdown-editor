402cdd7bdc562605e5efa150b82d41e4d0f4e8e2
Hao Sun <sunhao@easycorp.ltd>
2022-03-28 13:40:14 +0800

    Allow user to configure marked options
    pick from https://github.com/sparksuite/simplemde-markdown-editor/pull/366

--- src/js/simplemde.js
+++ src/js/simplemde.js
@@ -1392,7 +1392,12 @@ function SimpleMDE(options) {
 SimpleMDE.prototype.markdown = function(text) {
 	if(marked) {
 		// Initialize
-		var markedOptions = {};
+		var markedOptions;
+		if(this.options && this.options.renderingConfig && this.options.renderingConfig.markedOptions) {
+			markedOptions = this.options.renderingConfig.markedOptions;
+		} else {
+			markedOptions = {};
+		}
 
 
 		// Update options
